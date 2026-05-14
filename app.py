75abfbca739bc696a80afe2d2c8d0629778c1598
RajaSardar-11225 <dme1@safed.in>
2026-05-14 16:46:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import pymssql
+
+server = 'sdplserver.database.windows.net'
+database = 'dme'
+user_name = 'dmeadmin'
+password = 'Agri@774Safe#14'
+
+########################################################################
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+
+def home():
+    result = []
+    columns = []
+
+    if request.method=="POST":
+        user_question = request.form["query"]
+        if not user_question.lower().startswith("select"):
+            return "You Can Only Search your request. cant Modify."
+        
+        try:
+            now_connect = pymssql.connect(server=server, user= user_name, password= password, database= database)
+            cursor = now_connect.cursor()
+            cursor.execute(user_question)
+            result = cursor.fetchall()
+
+            columns = [column[0] for column in cursor.description]
+            now_connect.close()
+
+        except:
+            return "Please Provide correct Code"
+        
+    return render_template("index.html", result=result, columns=columns)
+
+if __name__=="__main__":
+    app.run(debug=True)
